2b2f2118b2d682b8d2cc68d3c2902154471778d7
Huthaifa-Dev <huthaifajamal2@gmail.com>
2022-05-12 13:34:01 +0300

    added python algo

---
+++ src/fib.py
@@ -0,0 +1,148 @@
+import json 
+
+class Node():
+    """A node class for A* Pathfinding"""
+
+    def __init__(self, parent=None, position=None):
+        self.parent = parent
+        self.position = position
+
+        self.g = 0
+        self.h = 0
+        self.f = 0
+
+    def __eq__(self, other):
+        return self.position == other.position
+
+
+def astar(maze, start, end):
+    """Returns a list of tuples as a path from the given start to the given end in the given maze"""
+
+    # Create start and end node
+    start_node = Node(None, start)
+    start_node.g = start_node.h = start_node.f = 0
+    end_node = Node(None, end)
+    end_node.g = end_node.h = end_node.f = 0
+
+    # Initialize both open and closed list
+    open_list = []
+    closed_list = []
+
+    # Add the start node
+    open_list.append(start_node)
+
+    # Loop until you find the end
+    while len(open_list) > 0:
+
+        # Get the current node
+        current_node = open_list[0]
+        current_index = 0
+        for index, item in enumerate(open_list):
+            if item.f < current_node.f:
+                current_node = item
+                current_index = index
+
+        # Pop current off open list, add to closed list
+        open_list.pop(current_index)
+        closed_list.append(current_node)
+
+        # Found the goal
+        if current_node == end_node:
+            path = []
+            current = current_node
+            while current is not None:
+                # print(current.position)
+                path.append(current.position)
+                current = current.parent
+            return path[::-1] # Return reversed path
+        # print(current_node.position)
+        # Generate children
+        children = []
+        for new_position in [(0, -1), (0, 1), (-1, 0), (1, 0)]: # Adjacent squares
+            
+            # Get node position
+            node_position = (current_node.position[0] + new_position[0], current_node.position[1] + new_position[1])
+            # print(node_position)
+            # Make sure within range
+            if node_position[0] > (len(maze) - 1) or node_position[0] < 0 or node_position[1] > (len(maze[len(maze)-1]) -1) or node_position[1] < 0:
+                continue
+
+            # Make sure walkable terrain
+            if maze[node_position[0]][node_position[1]] != 0:
+                continue
+            # print(node_position)
+            # Create new node
+            new_node = Node(current_node, node_position)
+
+            # Append
+            # print(node_position)
+            children.append(new_node)
+
+        # Loop through children
+        for child in children:
+            # Child is on the closed list
+            for closed_child in closed_list:
+                if child == closed_child:
+                    continue
+
+            # Create the f, g, and h values
+            child.g = current_node.g + 1
+            child.h = ((child.position[0] - end_node.position[0]) ** 2) + ((child.position[1] - end_node.position[1]) ** 2)
+            child.f = child.g + child.h
+
+            # Child is already in the open list
+            for open_node in open_list:
+                if child == open_node and child.g > open_node.g:
+                    continue
+
+            # Add the child to the open list
+            # print(child.g)
+            print(child.position,child.f,child.g)
+
+            open_list.append(child)
+
+
+
+def main():
+
+    maze = [[2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2],
+                [2,0,0,0,0,0,0,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,2,2],
+                [2,2,2,2,2,2,2,2,2,0,2,0,2,2,0,2,2,2,2,0,2,2,0,0,2],
+                [2,0,2,0,0,0,0,0,0,0,2,0,2,2,0,2,0,0,0,0,0,2,0,2,2],
+                [2,0,2,0,2,2,2,2,2,2,2,0,2,2,0,2,2,2,2,2,0,2,0,0,2],
+                [2,0,2,0,2,0,0,0,2,0,0,0,0,0,0,0,0,0,0,2,0,2,0,2,2],
+                [2,0,2,0,2,0,2,0,0,0,2,0,0,0,2,2,2,2,0,2,0,2,0,0,2],
+                [2,0,2,0,2,2,2,2,2,2,2,2,2,0,0,0,0,2,0,2,2,2,0,2,2],
+                [2,0,2,0,0,0,0,0,0,0,0,0,2,0,2,2,0,2,0,0,0,0,0,0,2],
+                [2,0,2,2,2,2,0,2,2,2,2,0,2,0,2,2,0,2,0,2,2,2,0,2,2],
+                [2,0,0,0,0,2,0,2,1,1,2,0,2,0,0,0,0,2,0,2,1,2,0,0,2],
+                [2,0,2,2,0,2,0,2,1,1,2,0,2,0,2,2,2,2,0,2,2,2,0,2,2],
+                [2,0,2,2,0,2,0,2,1,1,2,0,2,0,2,0,0,0,0,0,0,0,0,0,2],
+                [2,0,2,2,2,2,0,2,2,2,2,0,2,0,2,0,2,2,2,0,2,2,2,0,2],
+                [2,0,0,0,0,0,0,0,2,2,0,0,2,2,2,0,2,0,2,0,2,1,2,0,2],
+                [2,0,2,2,2,2,0,0,0,0,0,0,0,0,0,0,2,0,2,0,2,2,2,0,2],
+                [2,0,2,2,2,2,0,0,2,2,2,2,2,2,2,0,2,0,0,0,0,0,0,0,2],
+                [2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2]]
+    print(maze)       
+    for array in maze:
+        new_row = [1 if x == 2 else 0 for x in array]
+        print(new_row)
+        maze[maze.index(array)]= new_row
+    print(maze)
+    start = (1, 1)
+    end = (3, 5)
+
+    path = astar(maze, start, end)
+    print(path)
+
+    data = {
+        "maze":maze,
+        "solution":path
+    }
+
+    with open('data.json','w') as f:
+        json.dump(data,f)
+
+
+if __name__ == '__main__':
+    main()
